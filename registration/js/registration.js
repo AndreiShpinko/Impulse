d0e13e9883803b773b89a53f3f2e1a953b3c9632
AndreiShpinko <73303318+AndreiShpinko@users.noreply.github.com>
2021-04-28 09:29:26 +0300

    Add files via upload

--- registration/js/registration.js
+++ registration/js/registration.js
@@ -7,4 +7,43 @@ $(document).ready(function () {
     $(".header__burger, .header__menu, .header").removeClass("active");
     $(".body").removeClass("lock");
   });
+
+  const form = document.getElementById('form');
+  form.addEventListener('submit', formSend);
+
+  async function formSend(e) {
+    e.preventDefault();
+
+    let error = formValidate(form);
+  }
+
+  function formValidate(form) {
+    let error = 0;
+    let formReq = document.querySelectorAll('._req');
+  
+    for (let index = 0; index < formReq.length; index++) {
+      const input = formReq[index];
+      formRemoveError(input);
+
+      if(input.classList.contains('_phone')) {
+        if(phoneTest(input)) {
+          formAddError(input);
+          error++;
+        }
+      }
+    }
+    return error;
+  }
+
+  function formAddError(input) {
+    input.parentElement.classList.add('_error');
+    input.classList.add('_error');
+  }
+  function formRemoveError(input) {
+    input.parentElement.classList.remove('_error');
+    input.classList.remove('_error');
+  }
+  function phoneTest(input) {
+    return !/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/.test(input.value);
+  }
 });
